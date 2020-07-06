2e5c3bd72b7bb8038de2af325eb5fed6ce4df31d
angrarof <angel.aguilar0@hotmail.com>
2020-07-06 17:46:16 -0500

    Adding more code into new branch

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -46,15 +44,50 @@ public class FirstTest {
         driver.navigate().back();
 
         //Context Menu
-        driver.findElement(By.xpath("//*[contains(text(),'Context Menu')]")).click();//This element is imposible to click searching by css
+        /*driver.findElement(By.xpath("//*[contains(text(),'Context Menu')]")).click();//This element is imposible to click searching by css
         Assert.assertTrue(driver.findElement(By.cssSelector(".example")).isDisplayed());
 
-        Actions action = new Actions(driver);
+         Actions action = new Actions(driver);
         WebElement box = driver.findElement(By.cssSelector("#hot-spot"));
         action.contextClick(box).perform();
 
         Alert alert = driver.switchTo().alert();
         alert.accept();
+
+        driver.navigate().back();*/
+
+        //Digest autentication
+        driver.get("http://admin:admin@the-internet.herokuapp.com/digest_auth");
+        String message = driver.findElement(By.cssSelector("p")).getText();
+        System.out.println(message);
+        driver.navigate().back();
+
+        //Drag and drop --Note: Does not work for some reason
+        /*driver.findElement(By.xpath("//*[contains(text(),'Drag and Drop')]")).click();
+        WebElement from = driver.findElement(By.xpath("//*[@id='column-a']"));
+        WebElement to = driver.findElement(By.xpath("//*[@id='column-b']"));
+
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(from,to).build().perform();
+
+        driver.navigate().back();*/
+
+        //Multiple windows
+        driver.findElement(By.xpath("//*[contains(text(),'Multiple Windows')]")).click();
+        String parentWindow = driver.getWindowHandle();
+        driver.findElement(By.cssSelector("#content a")).click();
+
+        for (String winHandle : driver.getWindowHandles()){
+            if (!winHandle.equalsIgnoreCase(parentWindow)){
+                System.out.println("New window open");
+                driver.switchTo().window(winHandle);
+                Assert.assertTrue(driver.findElement(By.cssSelector(".example")).isDisplayed());
+                driver.close();
+            }
+        }
+
+        driver.switchTo().window(parentWindow);
+        System.out.println("Switched back to parent window");
 
         driver.navigate().back();
         /*------------------End my own test------------------*/
